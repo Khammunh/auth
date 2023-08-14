d4e3aac811728d31c757ee9617c96b8028514b7c
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-08-14 21:16:04 +0700

    AppBar otp.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:otp/otp.dart';
+import 'package:otp/phone.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: 'phone',
     routes: {
-      'phone': (context) => MyPhone(),
+      'phone': (context) =>const MyPhone(),
       'otp': (context) =>const MyOtp(),
     },
   ));
--- lib/otp.dart
+++ lib/otp.dart
@@ -1,10 +1,123 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_print, sort_child_properties_last, deprecated_member_use
 
-class MyOtp extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+
+class MyOtp extends StatefulWidget {
   const MyOtp({super.key});
 
   @override
+  State<MyOtp> createState() => _MyOtpState();
+}
+
+class _MyOtpState extends State<MyOtp> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 56,
+      textStyle: const TextStyle(
+          fontSize: 20,
+          color: Color.fromRGBO(30, 60, 87, 1),
+          fontWeight: FontWeight.w600),
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+
+    final focusedPinTheme = defaultPinTheme.copyDecorationWith(
+      border: Border.all(color: const Color.fromRGBO(114, 178, 238, 1)),
+      borderRadius: BorderRadius.circular(8),
+    );
+
+    final submittedPinTheme = defaultPinTheme.copyWith(
+      decoration: defaultPinTheme.decoration?.copyWith(
+        color: const Color.fromRGBO(234, 239, 243, 1),
+      ),
+    );
+
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+            //Now, I learn here.
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(left: 25, right: 25),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/img1.png',
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(height: 25),
+              const Text(
+                'Phone Verification',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'We need to register your phone before getting started !',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+              Pinput(
+                length: 6,
+                showCursor: true,
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 40,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Verify phone number'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green.shade600,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Edit Phone Number?',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
